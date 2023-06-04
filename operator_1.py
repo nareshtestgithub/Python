c5f0f39e6f8e0ada23079eac6c9dea1e0423e2f5
Naresh Khatri <111528185+nareshtestgithub@users.noreply.github.com>
2023-06-04 22:09:08 +0545

    the code of operator

---
+++ operator_1.py
@@ -0,0 +1,9 @@
+# Arithmetic operator
+
+number1 = int(input('enter the first value:'))
+number2 = int(input('enter the second value:'))
+
+Sum = number1 + number2
+
+print(' the sum of two number is', Sum)
+
